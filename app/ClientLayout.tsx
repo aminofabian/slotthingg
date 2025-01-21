5b24315d6d378b5c63f8013bce0f3ff57462ea02
Fabian Amino <aminofab@gmail.com>
2025-01-21 13:53:04 +0300

    fixed a few type errors

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,23 +1,21 @@
 'use client';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { SocketProvider } from '@/app/contexts/SocketContext';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/lib/query';
+import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { store } from './store/store';
+import { queryClient } from '@/lib/query';
 
-export default function ClientLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const inter = Inter({ subsets: ['latin'] });
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <SocketProvider>
+        <div className={inter.className}>
           {children}
-          <Toaster position="top-right" />
-        </SocketProvider>
+          <Toaster position="top-center" />
+        </div>
       </QueryClientProvider>
     </Provider>
   );
