0259e504ab779d57c2ab11e0620c7b6b40244231
Fabian Amino <aminofab@gmail.com>
2025-02-17 13:55:15 +0300

    added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,9 @@
-import SignUp from '@/app/components/Auth/SignUp';
+import SimpleSignUp from '@/app/components/Auth/SimpleSignUp';
 
 export default function RegisterPage() {
   return (
     <main className="min-h-screen bg-[#002222]">
-      <SignUp />
+      <SimpleSignUp />
     </main>
   );
 } 
