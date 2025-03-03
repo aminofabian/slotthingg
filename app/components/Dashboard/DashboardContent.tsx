bf9297fd81012f85834bdad21893825b2a0cde36
Fabian Amino <aminofab@gmail.com>
2025-03-03 13:04:24 +0300

    increased the game listings in a row from 5 to 6

--- app/components/Dashboard/DashboardContent.tsx
+++ app/components/Dashboard/DashboardContent.tsx
@@ -318,7 +318,7 @@ export default function DashboardContent() {
               </button>
             </div>
           ) : (
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
               {games.map((game) => (
                 <button
                   key={game.id}
